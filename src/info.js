671904e1ea1e84ac9f5d58e5e630fc00bc25a14b
Chae Seong Lim <pan1129@naver.com>
2023-10-19 01:34:56 +0900

    useMemo 사용방법 커밋

--- src/info.js
+++ src/info.js
@@ -1,41 +1,41 @@
-import { useEffect, useState } from "react";
+import { useReducer } from "react";
 
-const Info = () => {
-    const [name, setName] = useState('');
-    const [nickName, setNickname] = useState('');
-    useEffect(() => {
-        console.log('effect');
-        console.log(name);
-        return () => {
-            console.log('cleanup');
-            console.log(name);
+    
+    function reducer(state, action) {
+        return {
+            ...state,
+            [action.name]: action.value
         };
-    }, [name]);
+    }
 
-    const onChangeName = e => {
-        setName(e.target.value);
-    };
-
-    const onChangeNickname = e => {
-        setNickname(e.target.value);
-    };
-
-    return(
+    const Info = () => {
+        const [state, dispatch] = useReducer(reducer, {
+            name: '',
+            nickname: ''
+        });
+        const { name, nickname } = state;
+        const onChange = e => {
+            dispatch(e.target);
+        };
+    
+    return (
         <div>
             <div>
-                <input value={name} onChange={onChangeName} />
-                <input value={nickName} onChange={onChangeNickname} />
+                <input name="name" value={name} onChange={onChange} />
+                <input name="nickname" value={nickname} onChange={onchange} />
             </div>
             <div>
                 <div>
-                    <b>이름: </b> {name}
+                    <b>이름:</b> {name}
                 </div>
                 <div>
-                    <b>닉네임:</b> {nickName}
+                    <b>닉네임:</b>
+                    {nickname}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+
+};
 
 export default Info;
