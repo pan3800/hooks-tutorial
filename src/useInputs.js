ba7d50de17e6cedd5939c7d857c14328594d016f
Chae Seong Lim <pan1129@naver.com>
2023-10-19 02:06:19 +0900

    useInputs 사용방법

---
+++ src/useInputs.js
@@ -0,0 +1,17 @@
+import { useReducer } from "react";
+
+function reducer(state, action) {
+    return {
+        ...state,
+        [action.name]: action.value
+    };
+}
+
+export default function useInputs(initalForm) {
+    const [state, dispatch] = useReducer(reducer, initalForm);
+
+    const onChange = e => {
+        dispatch(e.target);
+    };
+    return [state, onChange];
+}
